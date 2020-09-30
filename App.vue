828546d67c2a47e2b312b96a3e48f472de5e7764
Bilal Lkhlouf <49133141+blkhlouf1@users.noreply.github.com>
2020-09-30 22:01:07 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+    <active-user :userName="user" :age="userAge"></active-user>
+    <user-data @update-userage="updateAge" @update-username="updateUser"></user-data>
+</template>
+
+
+<script>
+export default {
+    data() {
+        
+    return {user: "Bilal", userAge: "28"};
+    },
+    methods: {
+        updateUser(name){
+            this.user = name;
+        },
+        updateAge(age){
+            this.userAge = age;
+
+        }
+    }
+    
+
+}
+</script>
---
+++ UserData.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" v-model="userName" @input="updateUserName">
+    </div>
+    <div>
+        <label for="age">Age</label>
+        <input type="text" name="age" v-model="userAge" @input="$emit('update-userage',this.userAge)">
+    </div>
+ 
+</template>
+
+
+<script>
+export default {
+    emits: ['update-userage', 'update-username'],
+    data() {
+        return {userName: '', userAge: ''};
+    
+    },
+    methods:{
+        updateUserAge(){
+            this.$emit('update-userage',this.userAge);
+        },
+        updateUserName(){
+            this.$emit('update-username',this.userName)
+        }
+    }   
+}
+</script>
